64b81f90a204012b69e454fc0d9c01cfaf1566e4
Manoj Kumar Tiwari <manoj.tiwari@ptw-i.com>
2018-04-26 23:22:45 +0530

    added template nav bar which needs to be edited

--- website.js
+++ website.js
@@ -1,13 +1,29 @@
 var express = require('express');
 var app = express();
+var router = express.Router();
 
-app.get('/', function (req, res) {
+var path = __dirname + '/views';
 
-	res.send('<h1>Open Source For You!</h1>');
+app.use('/', router);
+
+router.get('/', function (req, res) {
+	res.sendFile(path + '/index.html');
+});
+
+router.get('/product', function (req, res) {
+	res.sendFile(path + '/product.html');
+});
+
+router.get('/about', function (req, res) {
+	res.sendFile(path + '/about.html');
+});
+
+app.use('*', function(req, res){
+	res.send('Error 404: Not Found!');
 });
 
 app.listen(3000, function () {
-console.log('Example app listening on port 3000!');
+console.log('Server running at port 3000!');
 });
 
 
